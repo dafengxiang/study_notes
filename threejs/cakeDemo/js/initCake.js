cf4c2bea4f9a339c8aa9775499e4d0caee8e4d52
wfx <45392461+dafengxiang@users.noreply.github.com>
2022-02-18 18:09:27 +0800

    Update initCake.js

--- threejs/cakeDemo/js/initCake.js
+++ threejs/cakeDemo/js/initCake.js
@@ -2,14 +2,12 @@
  * @Description: 初始化蛋糕
  * @Author: wangfengxiang
  * @Date: 2022-02-17 15:53:49
- * @LastEditTime: 2022-02-18 13:35:20
+ * @LastEditTime: 2022-02-18 13:37:28
  * @LastEditors: wangfengxiang
  */
 import loadSkin from './loadSkin.js'
 export default async function ({ rotate, size }) {
     const cakeGroup = new THREE.Group()
-    // 蛋糕主体
-    const cakeBody = new THREE.CylinderGeometry(50, 50, 40, 100, 1, false, 0, Math.PI * size);
 
     // 纹理
     const cakeSkin = new THREE.MeshLambertMaterial({
@@ -19,6 +17,8 @@ export default async function ({ rotate, size }) {
         map: await loadSkin('/cakeDemo/images/inner_skin.jpeg')
     })
 
+    // 蛋糕主体
+    const cakeBody = new THREE.CylinderGeometry(50, 50, 40, 100, 1, false, 0, Math.PI * size);
     cakeGroup.add(new THREE.Mesh(cakeBody, cakeSkin))
 
     // 蛋糕切面
