4a6af3b9da517a79aaf81149a655fdbe34ebc865
wfx <45392461+dafengxiang@users.noreply.github.com>
2022-02-24 18:36:46 +0800

    build: webpack构建

---
+++ threejs/cakeDemo/webpack.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Author: wangfengxiang
+ * @Date: 2022-02-24 18:23:57
+ * @LastEditTime: 2022-02-24 18:32:54
+ * @LastEditors: wangfengxiang
+ */
+module.exports={
+    entry:'./src/index.js'
+}
