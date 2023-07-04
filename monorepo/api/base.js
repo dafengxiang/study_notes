331aca78cc60b242aa39e2f78eb40b8e671c90c4
dafengxiang <1205679862@qq.com>
2023-07-04 23:54:27 +0800

    feat: monorepo公共api使用

---
+++ monorepo/api/base.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: 
+ * @Author: wangfengxiang
+ * @Date: 2023-07-04 23:33:56
+ * @LastEditTime: 2023-07-04 23:35:33
+ * @LastEditors: wangfengxiang
+ */
+import axios from 'axios'
+let Axios = axios.create({
+    baseURL:'/'
+})
+
+export default Axios
