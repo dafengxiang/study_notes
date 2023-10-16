d981073500aef0a028f9019aa2b73a80757f6974
王凤祥 <1205679862@qq.com>
2023-10-16 09:59:32 +0800

    feat: 导入文件

--- LangChain/ingest-data.js
+++ LangChain/ingest-data.js
@@ -2,8 +2,13 @@
  * @Description: 数据摄取
  * @Author: wangfengxiang
  * @Date: 2023-10-14 22:42:42
- * @LastEditTime: 2023-10-14 22:49:44
+ * @LastEditTime: 2023-10-16 09:59:16
  * @LastEditors: wangfengxiang
  */
-import {UnstructuredLoader} from 'langchain/document_loaders/fs/unstructured'
-const unstructuredLoader = new UnstructuredLoader()
+
+import { UnstructuredLoader } from 'langchain/document_loaders/fs/unstructured'
+console.log('UnstructuredLoader: ', UnstructuredLoader);
+const unstructuredLoader = new UnstructuredLoader('./vue3.md')
+console.log('unstructuredLoader: ', unstructuredLoader);
+const docs = await unstructuredLoader.load()
+console.log('docs: ', docs);
